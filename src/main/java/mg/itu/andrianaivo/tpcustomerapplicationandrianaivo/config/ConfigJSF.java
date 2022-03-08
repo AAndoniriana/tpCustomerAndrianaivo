82a3eede678ab2668f605c508811237112d416a4
Andrianaivo Andoniriana Andrianina <andrian.andrianaivo@gmail.com>
2022-03-08 18:57:44 +0300

    Config JSF

---
+++ src/main/java/mg/itu/andrianaivo/tpcustomerapplicationandrianaivo/config/ConfigJSF.java
@@ -0,0 +1,18 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package mg.itu.andrianaivo.tpcustomerapplicationandrianaivo.config;
+
+import javax.enterprise.context.ApplicationScoped;
+import javax.faces.annotation.FacesConfig;
+
+/**
+ *
+ * @author JAVA
+ */
+@ApplicationScoped
+@FacesConfig
+public class ConfigJSF {
+    
+}
---
+++ src/main/java/mg/itu/andrianaivo/tpcustomerapplicationandrianaivo/managedBeans/CustomerMBean.java
@@ -0,0 +1,46 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/JSF/JSFManagedBean.java to edit this template
+ */
+package mg.itu.andrianaivo.tpcustomerapplicationandrianaivo.managedBeans;
+
+import javax.inject.Named;
+import javax.faces.view.ViewScoped;
+import java.io.Serializable;
+import java.util.List;
+import javax.ejb.EJB;
+import mg.itu.andrianaivo.tpcustomerapplicationandrianaivo.entities.Customer;
+import mg.itu.andrianaivo.tpcustomerapplicationandrianaivo.session.CustomerManager;
+
+/**
+ *
+ * @author JAVA
+ */
+@Named(value = "customerMBean")
+@ViewScoped
+public class CustomerMBean implements Serializable {
+    
+    private List<Customer> customerList;
+
+    @EJB
+    private CustomerManager customerManager;
+
+    /**
+     * Creates a new instance of CustomerMBean
+     */
+    public CustomerMBean() {
+    }
+    
+    /**
+     * Fonction qui retourne la liste des clients pour affichage dans une DataTable
+     * 
+     * @return Liste des clients
+     */
+    public List<Customer> getCustomers() {
+        if (customerList == null) {
+            customerList = customerManager.getAllCustomer();
+        }
+        return customerList;
+    }
+    
+}
